f96de6fd432e6c080d86b7778da189e09a740660
Andreas Copan <avcopan@gmail.com>
2025-03-31 17:29:26 -0400

    Cleans up and adds independent Pixi environment
    Removes deprecated `autoreact` code and adds model-specific Pixi
    environment.

--- autoreact/params/__init__.py
+++
@@ -1,9 +0,0 @@
-""" Functions for dealing with kinetic parametrizations
-"""
-from autoreact.params._params import RxnParams
-from autoreact.params._params import multiply_factor
-
-__all__ = [
-    'RxnParams',
-    'multiply_factor',
-]
